e4d6cb1d35d48013d63d412ac937ca79f468f831
DmytroPutrin <dmytro.putrin@solarwinds.com>
2021-08-01 17:03:50 +0300

    App.tsx set basic structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+      <div>
+        <h1>Home</h1> <br/>
+        <a href="/hotels">Hotels List</a>
+      </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Hotels.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hotels() {
+  return (
+      <div>
+        <h1>Hotels!</h1> <br/>
+        <a href="/">Home</a>
+      </div>
+  );
+}
+
+export default Hotels;
