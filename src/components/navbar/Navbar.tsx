e1d1ebe36290a94a62975f52be46b4c649e63332
DmytroPutrin <dmytro.putrin@solarwinds.com>
2021-08-02 09:30:08 +0300

    Navbar.tsx add key attribute

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
+import { getHotels } from '../../api/api';
 
 const routes = [{path: '/', name: 'Home'}, { path: '/hotels', name: 'Hotels', }];
 
 function Navbar() {
-    const items = routes.map((route) => <li><NavLink exact to={route.path}>{route.name}</NavLink></li>);
+    const items = routes.map((route, i) => <li key={i}><NavLink exact to={route.path}>{route.name}</NavLink></li>);
 
-  return (
+    return (
       <nav className='navbar'>
           <ul>
               {items}
           </ul>
       </nav>
-  );
+    );
 }
 
 export default Navbar;
