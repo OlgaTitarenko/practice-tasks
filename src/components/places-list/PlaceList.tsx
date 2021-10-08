fe464ba5c6ad94ff8fe1a1620a1ddc7fd333670b
OlgaTitarenko <kizuneworld@gmail.com>
2021-10-08 16:34:47 +0300

    frist result

--- src/components/places-list/PlaceList.tsx
+++ src/components/places-list/PlaceList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {getHotels, getApartments, getLoges} from '../../api/api';
+import React, { useEffect, useState } from 'react';
+import { getHotels, getApartments, getLoges } from '../../api/api';
 
 interface Hotel {
   id: string,
@@ -30,7 +30,6 @@ interface Apartment {
   rate: number
 }
 
-
 interface Lodge {
   id: string,
   index: number,
@@ -46,32 +45,86 @@ interface Lodge {
 }
 
 interface Place {
-  title: string,
-  address: string,
-  distance: number,
+  title: string;
+  address: string;
+  distance: number;
   description: string
 }
 
 function PlaceList() {
-  
-  const places: Place[] = [];
 
-  Promise.allSettled([getHotels(), getApartments(), getLoges()])
+  //const places: Place[] = [];
+  const [places, setPlaces] = useState<Place[]>([])
+
+  useEffect(() => {
+
+    Promise.allSettled([getHotels(), getApartments(), getLoges()])
       .then((values) => {
-        console.log(values[0].value)
-     
-     
-
+        console.log(values)
+        if (values[0].status === 'fulfilled' && values[0].value !== {}) {
+         
+            values[0].value.forEach((value: Hotel) => {
+              let v: Place = {
+                title: value.title,
+                address: value.address,
+                distance: value.distance,
+                description: value.about
+              }
+                setPlaces(oldValue => [...oldValue, v])
+            })
+          
+          
+        }
+        if (values[1].status === 'fulfilled' && values[1].value !== {}) {
+          
+            values[1].value.forEach((value: Apartment) => {
+              let v: Place = {
+                title: value.title,
+                address: value.address,
+                distance: value.distance,
+                description: value.description
+              }
+                setPlaces(oldValue => [...oldValue, v])
+            })
+          
+        }
+        if (values[2].status === 'fulfilled' && values[2].value !== {}) {
+          values[2].value.forEach((value: Lodge) => {
+            let v: Place = {
+              title: value.title,
+              address: value.address,
+              distance: value.distance,
+              description: value.description
+            }
+              setPlaces(oldValue => [...oldValue, v])
+          })
+        }
 
       }).catch(error => alert(error))
-  
 
 
-    return (
-      <div>
-          Render list of all places here
+  }, [])
+
+
+
+  return (
+    <div>
+      <div className="ui cards">
+        {
+          places.map((place, key) =>
+            <div className="ui card" key={key}>
+              <div className="content">
+                <div className="header">{place.title}</div>
+                <div className="meta">{place.distance}</div>
+                <div className="description">{place.description}</div>
+              </div>
+            </div>
+
+          )
+        }
       </div>
-    );
+    </div>
+  );
 }
 
 export default PlaceList;
